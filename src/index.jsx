f55198fb02aea753d8429357b17aae056d757a89
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-02-10 17:11:20 +0900

    [MOD] 파일 구조 전면 수정

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import ReactDom from "react-dom/client";
+import Main from './main';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main />);
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+//최상위 컴포넌트
+import App from './components/App';
+import HomePage from './pages/home/HomePage';
+function Main(){
+  return(
+    <App>
+      <HomePage />
+    </App>
+  )
+}
+
+export default Main;
