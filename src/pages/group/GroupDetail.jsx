c860291ad2650a0074619174c3f5a7ce21fd9e92
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-02-14 06:22:04 +0900

    [MOD] group 관련 코드 수정

--- src/pages/group/GroupDetail.jsx
+++ src/pages/group/GroupDetail.jsx
@@ -1,15 +1,28 @@
 import items from "../../mock/group.json";
-import React from "react";
+import {React, useEffect} from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 
+function GroupItem() {
+  const { id } = useParams(); 
+  const navigate = useNavigate();
+  const group = items.find((g) => g.id === parseInt(id));
+  console.log(group.imageUrl);
 
-function GroupItem({ item }) {
+  useEffect(() =>{
+    if(!group){
+     navigate("/error");
+    }
+  },[group, navigate]);
+
+  if (!group) return null; // 리다이렉트 되므로 화면 출력 없음
+
   return (
     <div className="GroupItem">
-      <img src={item.imageUrl} />
+      <img src={group.imageUrl} />
       <div>
-        <h1>{item.name}</h1>
-        <p>{item.introduction}</p>
+        <h1>{group.name}</h1>
+        <p>{group.introduction}</p>
       </div>
     </div>
   );
