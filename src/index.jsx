49dd490409732e655f8a4504dd227d202e62ac26
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-02-13 18:06:42 +0900

    [MOD] index.jsx랑 main.jsx 코드 수정

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import Main from "./main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(root.render(<Main />));
+root.render(<Main />);
