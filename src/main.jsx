9e8fa0dc9945e40c4b7491abc1ef74cd05180792
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-02-13 16:25:59 +0900

    [ADD] 회원가입 라우트 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import SignIn from './pages/admin/LoginPage';
-
+import SignUp from './pages/admin/SignUpPage';
 function Main(){
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signin" element={<SignIn/>} /> 
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
       </Routes>
     </BrowserRouter>
   )
