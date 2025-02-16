3c9de7801b1be78cb6f1bf1e41c4821ea8a41e9c
ParkSeohyeon <tjgus4394@gmail.com>
2025-02-16 15:26:12 +0900

    [MOD]댓글 페이지네이션이랑 댓글 비었을때 경고 출력

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -4,9 +4,8 @@ import styles from './ErrorPage.module.css';
 function ErrorPage() {
   return (
     <div className={styles.container}>
-      <div className={styles.errorCode}>404</div>
       <div className={styles.flowerIcon}>
-        <img src="flower-icon.png" alt="Flower" className={styles.flowerImg} />
+        <img src="../../assets/error.png" alt="404" className={styles.flowerImg} />
       </div>
       <div className={styles.message}>찾을 수 없는 페이지입니다.</div>
       <div className={styles.subMessage}>요청하신 페이지가 사라졌거나, 잘못된 경로를 이용하셨어요.</div>
