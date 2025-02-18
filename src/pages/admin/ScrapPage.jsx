73ed353c4fa9525e2f6d2c470d7387d9752a3816
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-02-19 05:32:23 +0900

    [MOD] 네비바로 인한 글 가려짐 해결

---
+++ src/pages/admin/ScrapPage.jsx
@@ -0,0 +1,10 @@
+import styles from "./ScrapPage.module.css";
+
+function ScrapPage() {
+  return (
+    <div className={styles.page}>
+      <h1>스크랩한 글 모아보기</h1>
+    </div>
+  );
+}
+export default ScrapPage;
