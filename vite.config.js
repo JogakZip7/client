934723212fe5cbc6837d1da8d5c3463d810a69ba
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-02-18 15:30:42 +0900

    [config파일 수정]

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/client/",
-  plugins: [react()]
+  plugins: [react()],
   // server: {
   //   port: 3000,
   //   proxy: {
-  //     "/api": { 
+  //     "/api": {
   //       target: "http://localhost:3000/",
   //       changeOrigin: true,
   //       rewrite: (path) => path.replace(/^\/api/, ''),
